2bce1e9aa2ef2d09ec858f50c9f3872087f4ef9a
Orive2023 <67405430+pritam377@users.noreply.github.com>
2025-02-07 11:22:17 +0530

    Added Footer

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Container, Row, Button } from "reactstrap";
 import { NavLink, Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
-import "../Header/header.css"
+import "../Header/header.css";
 
 const nav__links = [
   {
@@ -20,8 +20,26 @@ const nav__links = [
 ];
 
 const Header = () => {
+  const headerRef = useRef(null);
+  const stickyHeaderFunc = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+  };
+
+  useEffect(()=>{
+    stickyHeaderFunc()
+    return window.removeEventListener('scroll',stickyHeaderFunc)
+  })
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <Container>
         <Row>
           <div className="nav__wrapper d-flex align-item-center justify-content-between">
@@ -36,7 +54,14 @@ const Header = () => {
               <ul className="menu d-flex align-item-center gap-5">
                 {nav__links.map((item, index) => (
                   <li className="nav__item" key={index}>
-                    <NavLink to={item.path} className={navClass=> navClass.isActive ? 'active__link':''}>{item.display}</NavLink>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "active__link" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
                   </li>
                 ))}
               </ul>
@@ -44,13 +69,17 @@ const Header = () => {
             {/* ===========mennu ends======= */}
 
             <div className="nav__right d-flex align-items-center gap-4">
-                <div className="nav__btns d-flex align-items-center gap-4">
-                  <Button className="btn secondary__btn"><Link to="/login">Login</Link></Button>
-                  <Button className="btn primary__btn"><Link to="/register">Register</Link></Button>
-                </div>
-                <span className="mobile__menu">
-                  <i class="ri-menu-line"></i>
-                </span>
+              <div className="nav__btns d-flex align-items-center gap-4">
+                <Button className="btn secondary__btn">
+                  <Link to="/login">Login</Link>
+                </Button>
+                <Button className="btn primary__btn">
+                  <Link to="/register">Register</Link>
+                </Button>
+              </div>
+              <span className="mobile__menu">
+                <i class="ri-menu-line"></i>
+              </span>
             </div>
           </div>
         </Row>
